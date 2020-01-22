d8baed06e8149aa105206b2d56f8537799135d01
Edward Pang <53325250+epang22@users.noreply.github.com>
2020-01-22 00:39:10 -0500

    Update and rename 2014-matscijunior to 2014-matscijunior.m

--- _awards/2014-matscijunior.m
+++ _awards/2014-matscijunior.m
@@ -1,10 +1,10 @@
 ---
 title: "Outstanding MatSci Junior Award"
 collection: awards
-type: Award
+type: "Award"
 permalink: /awards/2014-matscijunior
-granter: Northwestern University Department of Materials Science and Engineering
-date: 2014
+granter: "Northwestern University Department of Materials Science and Engineering"
+date: 2014-01-01
 ---
 
 Awarded to the single best junior in the department
